cce586881632fe890cb06ed74702823fe60f56cb
vickymengar <vickymengar110403@gmail.com>
2023-12-06 11:07:58 -0600

    Tercer intento

--- pages/album-registrar.php
+++ pages/album-registrar.php
@@ -36,7 +36,7 @@
             
             <div class="form-row">
               <div class="form-group col-md-12">
-                <img src="./img/singer.png" class="img-fluid rounded mx-auto d-block" alt="Sin imagen" width="15%">
+                <img src="../img/no-images.png" class="img-fluid rounded mx-auto d-block" alt="Sin imagen" width="15%">
               </div>
             </div>
 
@@ -107,16 +107,7 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/vendor/holder.min.js"></script>
-    <script>
-      Holder.addTheme('thumb', {
-        bg: '#55595c',
-        fg: '#eceeef',
-        text: 'Thumbnail'
-      });
-    </script>
+    
   </body>
 
 </html>
